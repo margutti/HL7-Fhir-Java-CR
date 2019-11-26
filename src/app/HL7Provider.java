50946815de8c1d365d1d042092d4632527df915b
Sergio Roberto Margutti <margutti@softmidia.com.br>
2019-11-26 20:47:16 -0200

    Formatado código Tela/HL7Provider

--- src/app/HL7Provider.java
+++ src/app/HL7Provider.java
@@ -6,10 +6,8 @@ import java.util.ArrayList;
 //import org.apache.derby.tools.sysinfo;
 //import org.hl7.fhir.instance.model.api.IBaseResource;
 //import org.hl7.fhir.utilities.graphql.Parser;
-
 //import com.google.gson.Gson;
 //import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.dstu2.composite.AddressDt;
 //import ca.uhn.fhir.model.dstu2.composite.HumanNameDt;
@@ -25,7 +23,7 @@ import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 public class HL7Provider {
-	
+
 //	public static void main(String[] args) {
 //	
 //		System.out.println("Starting...");
@@ -35,156 +33,154 @@ public class HL7Provider {
 ////		System.out.println(searchPatient("Ronaldo",""));
 //		
 //	}
-	
-	public static ArrayList<String[]> searchPatient(String name, String surname, String id) {
-		
-		ArrayList<String[]> result = new ArrayList<String[]>();
-		
-		// We're connecting to a DSTU1 compliant server in this example
-		FhirContext ctx = FhirContext.forDstu2();
-		String serverBase = "http://fhirtest.uhn.ca/baseDstu2";
-		 
-		IGenericClient client = ctx.newRestfulGenericClient(serverBase);
-		 
-		//Faz busca
-		Bundle results;
-		if(!id.equals("")) {
-			results = client.search().byUrl("Patient?_id="+id).returnBundle(Bundle.class).execute();
-			
-		}else {
-				results = client.search()
-			      .forResource(Patient.class)
-			      .where(Patient.GIVEN.matches().value(name)).and(Patient.FAMILY.matches().value(surname))
-			      .returnBundle(Bundle.class)
-			      .execute();
-				
-		}
-				
+    public static ArrayList<String[]> searchPatient(String name, String surname, String id) {
+
+        ArrayList<String[]> result = new ArrayList<String[]>();
+
+        // We're connecting to a DSTU1 compliant server in this example
+        FhirContext ctx = FhirContext.forDstu2();
+        String serverBase = "http://fhirtest.uhn.ca/baseDstu2";
+
+        IGenericClient client = ctx.newRestfulGenericClient(serverBase);
+
+        //Faz busca
+        Bundle results;
+        if (!id.equals("")) {
+            results = client.search().byUrl("Patient?_id=" + id).returnBundle(Bundle.class).execute();
+
+        } else {
+            results = client.search()
+                    .forResource(Patient.class)
+                    .where(Patient.GIVEN.matches().value(name)).and(Patient.FAMILY.matches().value(surname))
+                    .returnBundle(Bundle.class)
+                    .execute();
+
+        }
+
 //		int i = 0;
-		for (Entry entry : results.getEntry()) {
-			
-			String[] row = new String[3];
-			
-			String jsonEncoded = ctx.newJsonParser().encodeResourceToString(entry.getResource());			
-			Patient patient = FhirContext.forDstu2().newJsonParser().parseResource(Patient.class, jsonEncoded);
-			
-			System.out.println(patient.getName().toString());
-			System.out.println(patient.getGender());
-			System.out.println(patient.getAddress());
-			
-			String formatedName = patient.getNameFirstRep().getGivenAsSingleString()+" "+patient.getNameFirstRep().getFamilyAsSingleString();
-			
-			String formatedAdderess = 
-					patient.getAddressFirstRep().getCountry()+ ", "+
-					patient.getAddressFirstRep().getCity()+ " "+
-					"("+patient.getAddressFirstRep().getPostalCode()+")";
+        for (Entry entry : results.getEntry()) {
 
-			String formatedID = patient.getId().getValue().toString().substring(8,13);	
-			
-			row[0] = formatedID;
-			row[1] = formatedName;
-			row[2] = formatedAdderess;
-			result.add(row);
+            String[] row = new String[3];
+
+            String jsonEncoded = ctx.newJsonParser().encodeResourceToString(entry.getResource());
+            Patient patient = FhirContext.forDstu2().newJsonParser().parseResource(Patient.class, jsonEncoded);
+
+            System.out.println(patient.getName().toString());
+            System.out.println(patient.getGender());
+            System.out.println(patient.getAddress());
+
+            String formatedName = patient.getNameFirstRep().getGivenAsSingleString() + " " + patient.getNameFirstRep().getFamilyAsSingleString();
+
+            String formatedAdderess
+                    = patient.getAddressFirstRep().getCountry() + ", "
+                    + patient.getAddressFirstRep().getCity() + " "
+                    + "(" + patient.getAddressFirstRep().getPostalCode() + ")";
+
+            String formatedID = patient.getId().getValue().toString().substring(8, 13);
+
+            row[0] = formatedID;
+            row[1] = formatedName;
+            row[2] = formatedAdderess;
+            result.add(row);
 //		i++;
-		}
-		
-		return result;
-	}
-	
-	public static String getPacient(String id, String encode) {
-		
-		String result = "";
-		
-		FhirContext ctx = FhirContext.forDstu2();
-		String serverBase = "http://fhirtest.uhn.ca/baseDstu2";
-		IGenericClient client = ctx.newRestfulGenericClient(serverBase);
-		 
-		//Faz busca pelos par√¢metros fornecidos
-		Bundle results;
-		results = client.search().byUrl("Patient?_id="+id).returnBundle(Bundle.class).execute();
-			
-		for (Entry entry : results.getEntry()) {
-			
-			
-			if(encode == "json")
-				result = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(entry.getResource());
-			
-			if(encode == "xml")
-				result = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(entry.getResource());
+        }
 
-		}
-		
-		return result;
-	}
-	
-	public static Patient createPacient(String nome, String sobrenome, String cep, String cidade, String pais) {
-		
-		//Cria endere√ßo
-		AddressDt endereco = new AddressDt();
-		endereco.setPostalCode(cep);
-		endereco.setCity(cidade);
-		endereco.setCountry(pais);
-			
-		ArrayList<AddressDt> listaEnderecos = new ArrayList<AddressDt>();
-		listaEnderecos.add(endereco);
-		
-		//Cria paciente
-		Patient patient = new Patient();
-		patient.setId(IdDt.newRandomUuid());
-		patient.addName()
-			.addFamily(sobrenome)
-			.addGiven(nome);
-		patient.setAddress(listaEnderecos);
-		
+        return result;
+    }
+
+    public static String getPacient(String id, String encode) {
+
+        String result = "";
+
+        FhirContext ctx = FhirContext.forDstu2();
+        String serverBase = "http://fhirtest.uhn.ca/baseDstu2";
+        IGenericClient client = ctx.newRestfulGenericClient(serverBase);
+
+        //Faz busca pelos par√¢metros fornecidos
+        Bundle results;
+        results = client.search().byUrl("Patient?_id=" + id).returnBundle(Bundle.class).execute();
+
+        for (Entry entry : results.getEntry()) {
+
+            if (encode == "json") {
+                result = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(entry.getResource());
+            }
+
+            if (encode == "xml") {
+                result = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(entry.getResource());
+            }
+
+        }
+
+        return result;
+    }
+
+    public static Patient createPacient(String nome, String sobrenome, String cep, String cidade, String pais) {
+
+        //Cria endere√ßo
+        AddressDt endereco = new AddressDt();
+        endereco.setPostalCode(cep);
+        endereco.setCity(cidade);
+        endereco.setCountry(pais);
+
+        ArrayList<AddressDt> listaEnderecos = new ArrayList<AddressDt>();
+        listaEnderecos.add(endereco);
+
+        //Cria paciente
+        Patient patient = new Patient();
+        patient.setId(IdDt.newRandomUuid());
+        patient.addName()
+                .addFamily(sobrenome)
+                .addGiven(nome);
+        patient.setAddress(listaEnderecos);
+
 //		Parser p = new Parser();
-		
-		return patient;
-	}
-	
-	public static String[] insertPatient(Patient patient) {
-				
-		//Cria parser
-		
-		// Cria novo Bundle de entrada 
-		Bundle bundle = new Bundle();
-		bundle.setType(BundleTypeEnum.TRANSACTION);
+        return patient;
+    }
 
-		//Adiciona o paciente ao bundle de entrada
-		bundle.addEntry()
-		   .setFullUrl(patient.getId().getValue())
-		   .setResource(patient)
-		   .getRequest()
-		      .setUrl("Patient")
-		      .setMethod(HTTPVerbEnum.POST);
-		
-		// Cria contexto do banco
-		FhirContext ctx = FhirContext.forDstu2();
+    public static String[] insertPatient(Patient patient) {
+
+        //Cria parser
+        // Cria novo Bundle de entrada 
+        Bundle bundle = new Bundle();
+        bundle.setType(BundleTypeEnum.TRANSACTION);
+
+        //Adiciona o paciente ao bundle de entrada
+        bundle.addEntry()
+                .setFullUrl(patient.getId().getValue())
+                .setResource(patient)
+                .getRequest()
+                .setUrl("Patient")
+                .setMethod(HTTPVerbEnum.POST);
+
+        // Cria contexto do banco
+        FhirContext ctx = FhirContext.forDstu2();
 //		System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(bundle));
-		 
-		// Cria cliente do banco com contexto do banco
-		IGenericClient client = ctx.newRestfulGenericClient("http://fhirtest.uhn.ca/baseDstu2");
-		Bundle resp = client.transaction().withBundle(bundle).execute();
-		 
-		// loga e retorna o resultado da inserÁ„o
-		System.out.println("Criado paciente com id "+resp.getEntryFirstRep().getResponse().getLocation().substring(8,13));
-		System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(resp));
-		System.out.println(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(resp));
-		
-		String[] retornos = new String[3];
-		retornos[0] = resp.getEntryFirstRep().getResponse().getLocation().substring(8,13);
-		retornos[1] = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(resp);
-		retornos[2] = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(resp);
-		
-		return retornos;
-	}
-	
-	public static Patient createSimplePacient(String nome, String sobrenome) {
-		//Cria paciente
-		Patient patient = new Patient();
-		patient.setId(IdDt.newRandomUuid());
-		patient.addName()
-			.addFamily(sobrenome)
-			.addGiven(nome);
-		return patient;
-	}
+
+        // Cria cliente do banco com contexto do banco
+        IGenericClient client = ctx.newRestfulGenericClient("http://fhirtest.uhn.ca/baseDstu2");
+        Bundle resp = client.transaction().withBundle(bundle).execute();
+
+        // loga e retorna o resultado da inserÁ„o
+        System.out.println("Criado paciente com id " + resp.getEntryFirstRep().getResponse().getLocation().substring(8, 13));
+        System.out.println(ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(resp));
+        System.out.println(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(resp));
+
+        String[] retornos = new String[3];
+        retornos[0] = resp.getEntryFirstRep().getResponse().getLocation().substring(8, 13);
+        retornos[1] = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(resp);
+        retornos[2] = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(resp);
+
+        return retornos;
+    }
+
+    public static Patient createSimplePacient(String nome, String sobrenome) {
+        //Cria paciente
+        Patient patient = new Patient();
+        patient.setId(IdDt.newRandomUuid());
+        patient.addName()
+                .addFamily(sobrenome)
+                .addGiven(nome);
+        return patient;
+    }
 }
--- src/app/Tela.java
+++ src/app/Tela.java
@@ -26,227 +26,229 @@ import javax.swing.JTable;
 
 public class Tela extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField txtNome;
-	private JTextField txtSobrenome;
-	private JTextField TxtCEP;
-	private JTextField txtCidade;
-	private JTextField txtPais;
-	private JTextField txtIDPesquisa;
-	private JTextField txtNomePesquisa;
-	private JTextField txtSobrenomePesquisa;
-	private JLabel lblIdPesquisa;
-	private JTable table;
-	private JScrollPane scrollPane;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField txtNome;
+    private JTextField txtSobrenome;
+    private JTextField TxtCEP;
+    private JTextField txtCidade;
+    private JTextField txtPais;
+    private JTextField txtIDPesquisa;
+    private JTextField txtNomePesquisa;
+    private JTextField txtSobrenomePesquisa;
+    private JLabel lblIdPesquisa;
+    private JTable table;
+    private JScrollPane scrollPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Tela frame = new Tela();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Tela frame = new Tela();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public Tela() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 511, 675);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		setResizable(false);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 286, 475, 2);
-		contentPane.add(separator);
-		
-		JLabel lblPesquisa = new JLabel("Pesquisa");
-		lblPesquisa.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPesquisa.setFont(new Font("Calibri Light", Font.BOLD, 18));
-		lblPesquisa.setBounds(10, 286, 475, 34);
-		contentPane.add(lblPesquisa);
-		
-		JLabel lblCadastro = new JLabel("Cadastro");
-		lblCadastro.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCadastro.setFont(new Font("Calibri Light", Font.BOLD, 18));
-		lblCadastro.setBounds(10, 0, 475, 34);
-		contentPane.add(lblCadastro);
-		
-		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(81, 61, 86, 14);
-		contentPane.add(lblNome);
-		
-		JLabel lblSobrenome = new JLabel("Sobrenome:");
-		lblSobrenome.setBounds(81, 86, 86, 14);
-		contentPane.add(lblSobrenome);
-		
-		JLabel lblCEP = new JLabel("CEP:");
-		lblCEP.setBounds(81, 149, 86, 14);
-		contentPane.add(lblCEP);
-		
-		JLabel lblCidadde = new JLabel("Cidade:");
-		lblCidadde.setBounds(81, 174, 86, 14);
-		contentPane.add(lblCidadde);
-		
-		JLabel lblPas = new JLabel("País:");
-		lblPas.setBounds(81, 199, 86, 14);
-		contentPane.add(lblPas);
-		
-		JLabel lblEndereo = new JLabel("Endereço");
-		lblEndereo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblEndereo.setBounds(81, 124, 66, 14);
-		contentPane.add(lblEndereo);
-		
-		txtNome = new JTextField();
-		txtNome.setBounds(177, 58, 239, 20);
-		contentPane.add(txtNome);
-		txtNome.setColumns(10);
-		
-		txtSobrenome = new JTextField();
-		txtSobrenome.setColumns(10);
-		txtSobrenome.setBounds(177, 83, 239, 20);
-		contentPane.add(txtSobrenome);
-		
-		TxtCEP = new JTextField();
-		TxtCEP.setColumns(10);
-		TxtCEP.setBounds(177, 146, 239, 20);
-		contentPane.add(TxtCEP);
-		
-		txtCidade = new JTextField();
-		txtCidade.setColumns(10);
-		txtCidade.setBounds(177, 171, 239, 20);
-		contentPane.add(txtCidade);
-		
-		txtPais = new JTextField();
-		txtPais.setColumns(10);
-		txtPais.setBounds(177, 196, 239, 20);
-		contentPane.add(txtPais);
-		
-		JLabel lblDadosGerais = new JLabel("Dados Gerais");
-		lblDadosGerais.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblDadosGerais.setBounds(81, 39, 120, 14);
-		contentPane.add(lblDadosGerais);
-		
-		lblIdPesquisa = new JLabel("ID:");
-		lblIdPesquisa.setBounds(356, 334, 50, 14);
-		contentPane.add(lblIdPesquisa);
-		
-		JLabel label = new JLabel("Nome:");
-		label.setBounds(32, 337, 86, 14);
-		contentPane.add(label);
-		
-		JLabel label_1 = new JLabel("Sobrenome:");
-		label_1.setBounds(32, 365, 102, 14);
-		contentPane.add(label_1);
-		
-		txtIDPesquisa = new JTextField();
-		txtIDPesquisa.setColumns(10);
-		txtIDPesquisa.setBounds(389, 331, 68, 20);
-		contentPane.add(txtIDPesquisa);
-		
-		txtNomePesquisa = new JTextField();
-		txtNomePesquisa.setColumns(10);
-		txtNomePesquisa.setBounds(128, 334, 136, 20);
-		contentPane.add(txtNomePesquisa);
-		
-		txtSobrenomePesquisa = new JTextField();
-		txtSobrenomePesquisa.setColumns(10);
-		txtSobrenomePesquisa.setBounds(127, 362, 137, 20);
-		contentPane.add(txtSobrenomePesquisa);
-		
-		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String nome = txtNome.getText().toString();
-				String sobrenome = txtSobrenome.getText().toString();
-				String cep = TxtCEP.getText().toString();
-				String cidade = txtCidade.getText().toString();
-				String pais = txtPais.getText().toString();
-				
-				String[] msg = HL7Provider.insertPatient(HL7Provider.createPacient(nome, sobrenome, cep, cidade, pais));
-				
-			    String[] buttons = { "Ver JSON", "Ver XML", "Fechar" };
+    /**
+     * Create the frame.
+     */
+    public Tela() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 511, 675);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        setResizable(false);
 
-			    int rc = JOptionPane.showOptionDialog(null, "Paciente inserido com ID "+msg[0], "Sucesso",
-			        JOptionPane.PLAIN_MESSAGE, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[2]);
-				
-			    //JSON
-			    if(rc == 0) {
-			    	
-			    	String json = HL7Provider.getPacient(msg[0],"json");
-			    	JOptionPane.showMessageDialog(null,json, "JSON", JOptionPane.PLAIN_MESSAGE);
-			    }
-			    
-			    //XML
-			    if(rc == 1) {
-			    	String xml = HL7Provider.getPacient(msg[0],"xml");
-			    	JOptionPane.showMessageDialog(null,xml, "XML", JOptionPane.PLAIN_MESSAGE);
-			    }
-			}
-		});
-		btnCadastrar.setBounds(309, 241, 148, 23);
-		contentPane.add(btnCadastrar);
-		
-		JButton btnPesquisar = new JButton("Pesquisar");
-		btnPesquisar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				String id = txtIDPesquisa.getText().toString();
-				
-				String nome = null;
-				if(txtNomePesquisa.getText().toString()!= "")
-					nome = txtNomePesquisa.getText().toString();
+        JSeparator separator = new JSeparator();
+        separator.setBounds(10, 286, 475, 2);
+        contentPane.add(separator);
 
-				String sobrenome = null;
-				if(txtSobrenomePesquisa.getText().toString() != "")
-					sobrenome = txtSobrenomePesquisa.getText().toString();
-				
-				ArrayList<String[]> result = HL7Provider.searchPatient(nome, sobrenome, id);
-								
-				DefaultTableModel model = new DefaultTableModel(); 
-				JTable table = new JTable(model); 
-				
-				model.addColumn("ID"); 
-				table.getColumn("ID").setMaxWidth(15);
-				model.addColumn("Nome");
-				model.addColumn("Endereço"); 
-				
-				for (String[] row : result) {
-					model.addRow(row);
-				}
-				
-				scrollPane.setViewportView(table);
-			}
-		});
-		btnPesquisar.setBounds(375, 361, 110, 23);
-		contentPane.add(btnPesquisar);
-		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(20, 395, 465, 210);
-		contentPane.add(scrollPane);
-				
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		
-		JLabel lblInformticaBiomdica = new JLabel("Informática Biomédica | UFCSPA | Uso permitido para fins educacionais");
-		lblInformticaBiomdica.setHorizontalAlignment(SwingConstants.CENTER);
-		lblInformticaBiomdica.setBounds(10, 615, 471, 14);
-		contentPane.add(lblInformticaBiomdica);
-	}
+        JLabel lblPesquisa = new JLabel("Pesquisa");
+        lblPesquisa.setHorizontalAlignment(SwingConstants.CENTER);
+        lblPesquisa.setFont(new Font("Calibri Light", Font.BOLD, 18));
+        lblPesquisa.setBounds(10, 286, 475, 34);
+        contentPane.add(lblPesquisa);
+
+        JLabel lblCadastro = new JLabel("Cadastro");
+        lblCadastro.setHorizontalAlignment(SwingConstants.CENTER);
+        lblCadastro.setFont(new Font("Calibri Light", Font.BOLD, 18));
+        lblCadastro.setBounds(10, 0, 475, 34);
+        contentPane.add(lblCadastro);
+
+        JLabel lblNome = new JLabel("Nome:");
+        lblNome.setBounds(81, 61, 86, 14);
+        contentPane.add(lblNome);
+
+        JLabel lblSobrenome = new JLabel("Sobrenome:");
+        lblSobrenome.setBounds(81, 86, 86, 14);
+        contentPane.add(lblSobrenome);
+
+        JLabel lblCEP = new JLabel("CEP:");
+        lblCEP.setBounds(81, 149, 86, 14);
+        contentPane.add(lblCEP);
+
+        JLabel lblCidadde = new JLabel("Cidade:");
+        lblCidadde.setBounds(81, 174, 86, 14);
+        contentPane.add(lblCidadde);
+
+        JLabel lblPas = new JLabel("País:");
+        lblPas.setBounds(81, 199, 86, 14);
+        contentPane.add(lblPas);
+
+        JLabel lblEndereo = new JLabel("Endereço");
+        lblEndereo.setFont(new Font("Tahoma", Font.BOLD, 11));
+        lblEndereo.setBounds(81, 124, 66, 14);
+        contentPane.add(lblEndereo);
+
+        txtNome = new JTextField();
+        txtNome.setBounds(177, 58, 239, 20);
+        contentPane.add(txtNome);
+        txtNome.setColumns(10);
+
+        txtSobrenome = new JTextField();
+        txtSobrenome.setColumns(10);
+        txtSobrenome.setBounds(177, 83, 239, 20);
+        contentPane.add(txtSobrenome);
+
+        TxtCEP = new JTextField();
+        TxtCEP.setColumns(10);
+        TxtCEP.setBounds(177, 146, 239, 20);
+        contentPane.add(TxtCEP);
+
+        txtCidade = new JTextField();
+        txtCidade.setColumns(10);
+        txtCidade.setBounds(177, 171, 239, 20);
+        contentPane.add(txtCidade);
+
+        txtPais = new JTextField();
+        txtPais.setColumns(10);
+        txtPais.setBounds(177, 196, 239, 20);
+        contentPane.add(txtPais);
+
+        JLabel lblDadosGerais = new JLabel("Dados Gerais");
+        lblDadosGerais.setFont(new Font("Tahoma", Font.BOLD, 11));
+        lblDadosGerais.setBounds(81, 39, 120, 14);
+        contentPane.add(lblDadosGerais);
+
+        lblIdPesquisa = new JLabel("ID:");
+        lblIdPesquisa.setBounds(356, 334, 50, 14);
+        contentPane.add(lblIdPesquisa);
+
+        JLabel label = new JLabel("Nome:");
+        label.setBounds(32, 337, 86, 14);
+        contentPane.add(label);
+
+        JLabel label_1 = new JLabel("Sobrenome:");
+        label_1.setBounds(32, 365, 102, 14);
+        contentPane.add(label_1);
+
+        txtIDPesquisa = new JTextField();
+        txtIDPesquisa.setColumns(10);
+        txtIDPesquisa.setBounds(389, 331, 68, 20);
+        contentPane.add(txtIDPesquisa);
+
+        txtNomePesquisa = new JTextField();
+        txtNomePesquisa.setColumns(10);
+        txtNomePesquisa.setBounds(128, 334, 136, 20);
+        contentPane.add(txtNomePesquisa);
+
+        txtSobrenomePesquisa = new JTextField();
+        txtSobrenomePesquisa.setColumns(10);
+        txtSobrenomePesquisa.setBounds(127, 362, 137, 20);
+        contentPane.add(txtSobrenomePesquisa);
+
+        JButton btnCadastrar = new JButton("Cadastrar");
+        btnCadastrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+
+                String nome = txtNome.getText().toString();
+                String sobrenome = txtSobrenome.getText().toString();
+                String cep = TxtCEP.getText().toString();
+                String cidade = txtCidade.getText().toString();
+                String pais = txtPais.getText().toString();
+
+                String[] msg = HL7Provider.insertPatient(HL7Provider.createPacient(nome, sobrenome, cep, cidade, pais));
+
+                String[] buttons = {"Ver JSON", "Ver XML", "Fechar"};
+
+                int rc = JOptionPane.showOptionDialog(null, "Paciente inserido com ID " + msg[0], "Sucesso",
+                        JOptionPane.PLAIN_MESSAGE, JOptionPane.INFORMATION_MESSAGE, null, buttons, buttons[2]);
+
+                //JSON
+                if (rc == 0) {
+
+                    String json = HL7Provider.getPacient(msg[0], "json");
+                    JOptionPane.showMessageDialog(null, json, "JSON", JOptionPane.PLAIN_MESSAGE);
+                }
+
+                //XML
+                if (rc == 1) {
+                    String xml = HL7Provider.getPacient(msg[0], "xml");
+                    JOptionPane.showMessageDialog(null, xml, "XML", JOptionPane.PLAIN_MESSAGE);
+                }
+            }
+        });
+        btnCadastrar.setBounds(309, 241, 148, 23);
+        contentPane.add(btnCadastrar);
+
+        JButton btnPesquisar = new JButton("Pesquisar");
+        btnPesquisar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+
+                String id = txtIDPesquisa.getText().toString();
+
+                String nome = null;
+                if (txtNomePesquisa.getText().toString() != "") {
+                    nome = txtNomePesquisa.getText().toString();
+                }
+
+                String sobrenome = null;
+                if (txtSobrenomePesquisa.getText().toString() != "") {
+                    sobrenome = txtSobrenomePesquisa.getText().toString();
+                }
+
+                ArrayList<String[]> result = HL7Provider.searchPatient(nome, sobrenome, id);
+
+                DefaultTableModel model = new DefaultTableModel();
+                JTable table = new JTable(model);
+
+                model.addColumn("ID");
+                table.getColumn("ID").setMaxWidth(15);
+                model.addColumn("Nome");
+                model.addColumn("Endereço");
+
+                for (String[] row : result) {
+                    model.addRow(row);
+                }
+
+                scrollPane.setViewportView(table);
+            }
+        });
+        btnPesquisar.setBounds(375, 361, 110, 23);
+        contentPane.add(btnPesquisar);
+
+        scrollPane = new JScrollPane();
+        scrollPane.setBounds(20, 395, 465, 210);
+        contentPane.add(scrollPane);
+
+        table = new JTable();
+        scrollPane.setViewportView(table);
+
+        JLabel lblInformticaBiomdica = new JLabel("Informática Biomédica | UFCSPA | Uso permitido para fins educacionais");
+        lblInformticaBiomdica.setHorizontalAlignment(SwingConstants.CENTER);
+        lblInformticaBiomdica.setBounds(10, 615, 471, 14);
+        contentPane.add(lblInformticaBiomdica);
+    }
 }
